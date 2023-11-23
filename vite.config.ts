65494ca586a89d6c42406290bf367c1c61eeb67a
Érus Ferreira <erusferreira@gmail.com>
2023-11-23 10:43:09 -0300

    Remote microfrontend configs

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import federation from "@originjs/vite-plugin-federation";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    tsconfigPaths(),
+    federation({
+      name: "admin",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./CatalogAdmin": "./src/App",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    target: "esnext",
+  },
+});
