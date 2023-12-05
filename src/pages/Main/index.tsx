d036ca4aa1299faec92a419f418dc59169866597
Érus Ferreira <erusferreira@gmail.com>
2023-12-05 18:30:11 -0300

    Visuals improvements

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -9,7 +9,7 @@ export default function Main() {
   const [ catalogs, setCatalogs ] = useState([]);  
   const [ state ] = useAdminToken();
   const [ , setAuthorized ] = useAuthorized();
-  
+
   async function fetchApi() {
     try {
       const url = APIConfig.CATALOG.GET("654e427ca505d3d5790005e6");
@@ -22,7 +22,7 @@ export default function Main() {
       if (error.response.status == 401) {
         setAuthorized(false)
       }
-      throw new Error(`Erro ao carregar catálogos: ${error}`);
+      throw new Error(error);
     }
   }
   
@@ -32,32 +32,21 @@ export default function Main() {
   
   return (
     <>
-      <section className="bg-white py-8">
-        <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
-        <nav id="store" className="w-full z-30 top-0 px-6 py-1">
-          <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
-            <span className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl ">
-             Cardápios
-            </span>
-          </div>
-        </nav>
-        
+      <section className="bg-light-gray h-height w-screen flex m-auto">
         {catalogs.map((catalog: any) => (
-          <div key={catalog._id} className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col">
-            <a href="#">
-              <img
-                className="hover:grow hover:shadow-lg"
-                src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
-              />
-              <div className="pt-3 flex items-center justify-between">
-                <p className="">{catalog.name}</p>
+          <div className="bg-white m-3.5 w-6/12 shadow-box-shadow grid justify-items-center">
+              <div key={catalog._id}
+                   className="">
+                <img className="my-3.5"
+                     src="https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80"
+                />
+                <div className="pt-3 flex items-center justify-between">
+                  <p className="">{catalog.name}</p>
+                </div>
+                <p className="pt-1 text-gray-900">{catalog.description}</p>
               </div>
-              <p className="pt-1 text-gray-900">{catalog.description}</p>
-            </a>
           </div>
         ))}
-        
-        </div>
       </section>
     </>
   )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-gray': '#e2e8f0'
+      },
+      boxShadow: {
+        'box-shadow': 'rgba(0, 0, 0, 0.1) 0px 1px 3px 0px, rgba(0, 0, 0, 0.06) 0px 1px 2px 0px'
+      },
+      height: {
+        'height': 'calc(100vh - 64px)'
+      }
+    },
   },
   plugins: [],
 };
